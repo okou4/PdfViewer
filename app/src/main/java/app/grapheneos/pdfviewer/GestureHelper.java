f4fe05524c7066d82bfe9a52b7287698f39a92f4
okou <112856256+okou4@users.noreply.github.com>
2024-07-05 21:39:50 +0300

    Moved TapZone creation function to KtUtils and inside TapZone class

--- app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
+++ app/src/main/java/app/grapheneos/pdfviewer/GestureHelper.java
@@ -11,6 +11,7 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.RectKt;
 
 /*
     The GestureHelper present a simple gesture api for the PdfViewer
@@ -27,21 +28,6 @@ class GestureHelper {
         void onZoomEnd();
     }
 
-    static TapZones createTapZones(final int containerWidth) {
-        final float leftAndRightWidth = (float)(containerWidth * 0.4);  // Left and Right zones are 40% each
-        final float middleWidth = (float)(containerWidth * 0.2);        // Middle zone is 20%
-
-        final float leftZoneLeft = 0.0f;
-        final float middleZoneRight = leftAndRightWidth + middleWidth;
-        final float rightZoneRight = middleZoneRight + leftAndRightWidth;
-
-        final RectF leftZone = new RectF(leftZoneLeft, 0, leftAndRightWidth, 1);
-        final RectF middleZone = new RectF(leftAndRightWidth, 0, middleZoneRight, 1);
-        final RectF rightZone = new RectF(middleZoneRight, 0, rightZoneRight, 1);
-
-        return new TapZones(leftZone, middleZone, rightZone);
-    }
-
     @SuppressLint("ClickableViewAccessibility")
     static void attach(Context context, View gestureView, GestureListener listener) {
 
@@ -51,7 +37,7 @@ class GestureHelper {
                     public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
                         final int viewWidth = gestureView.getWidth();
                         final float tapX = motionEvent.getX();
-                        final TapZones zones = createTapZones(viewWidth);
+                        final TapZones zones = new TapZones(viewWidth);
 
                         if(zones.getLeft().contains(tapX, 0)) {
                             return listener.onTapLeft();
--- app/src/main/java/app/grapheneos/pdfviewer/KtUtils.kt
+++ app/src/main/java/app/grapheneos/pdfviewer/KtUtils.kt
@@ -30,4 +30,22 @@ private fun Context.asInputStream(uri: Uri): InputStream? = contentResolver.open
 @Throws(FileNotFoundException::class)
 private fun Context.asOutputStream(uri: Uri): OutputStream? = contentResolver.openOutputStream(uri)
 
-data class TapZones(val left: RectF, val middle: RectF, val right: RectF)
+class TapZones(containerWidth: Int) {
+    val left: RectF
+    val middle: RectF
+    val right: RectF
+
+    init {
+        // Left and Right zones are 40% each and while the Middle zone is 20%
+        val leftAndRightWidth = (containerWidth * 0.4).toFloat()
+        val middleWidth = (containerWidth * 0.2).toFloat()
+
+        val leftZoneLeft = 0.0f
+        val middleZoneRight = leftAndRightWidth + middleWidth
+        val rightZoneRight = middleZoneRight + leftAndRightWidth
+
+        left = RectF(leftZoneLeft, 0f, leftAndRightWidth, 1f)
+        middle = RectF(leftAndRightWidth, 0f, middleZoneRight, 1f)
+        right = RectF(middleZoneRight, 0f, rightZoneRight, 1f)
+    }
+}
